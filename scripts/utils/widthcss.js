f1cc0871d476d7d88bb1c4bd0e896ed70749d5ae
AkuaSin <aquanet200@gmail.com>
2022-03-10 22:00:26 +0100

    just a tweak in how many things before next width

--- scripts/utils/widthcss.js
+++ scripts/utils/widthcss.js
@@ -1,19 +1,19 @@
 function widthcss(){
     let sel= document.querySelector("#ingredientList")
     let list = document.querySelectorAll("#ingredientList li")
-    if(list.length < 28){
+    if(list.length < 27){
     sel.style.width = "173px"
     }
-    if(list.length >= 28){
+    if(list.length >= 27){
         sel.style.width = "323px"
         }
-    if(list.length >= 56){
+    if(list.length >= 54){
     sel.style.width = "473px"
     }
-    if(list.length >= 84){
+    if(list.length >= 81){
         sel.style.width = "623px"
         }
-    if(list.length >= 112){
+    if(list.length >= 108){
             sel.style.width = "773px"
         }
 }
